f03b28c594fb06ef839880d2f39e51673df577dd
Amiir25 <amirsadik636@gmail.com>
2026-09-13 02:00:13 +0300

    style: add color variants on the reusable button component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,16 +3,37 @@ import React from "react";
 const Button = ({
   children,
   onClick,
-  isActive = true,
-  primary = false,
+  color,
+  primary,
+  type
 }) => {
+  
+  // Dynamic button decoration
+  let btnDecore = "";
+  switch(color) {
+    case "white":
+      btnDecore = "bg-white text-dark-red ";
+      break;
+    case "light-red":
+      btnDecore = "bg-light-red text-dark-text";
+      break;
+    case "light-yellow":
+      btnDecore = "bg-light-yellow text-light-text";
+      break;
+    case "brand":
+      btnDecore = "bg-brand text-white w-full shadow-xl";
+      break;
+    default:
+      return "Button color not provided"
+  }
+
   return (
     <button
+      type={type ? "submit" : "button"}
+      onClick={onClick}
       className={
-        `py-[0.3em] px-[1em] m-2 rounded-lg text-lg hover:scale-110 active:scale-100 transition-scale duration-200
-        flex items-center justify-center gap-1
-        ${isActive ? "bg-[#9B2C16] text-white" : "bg-[#FCEAE4] text-[#58413D]"}
-        ${primary && 'w-full'}`
+        `py-[0.3em] px-[1em] rounded-lg hover:scale-105 active:scale-100 transition-scale duration-200
+        flex items-center gap-2 justify-center ${btnDecore} ${primary && 'w-full'} group`
       }
     >
       {children}
