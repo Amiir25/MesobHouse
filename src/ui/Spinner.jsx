ef0c52654b2e611d1e3f488f60a1d5e7ee854e4a
Amiir25 <amirsadik636@gmail.com>
2026-09-15 11:27:25 +0300

    refactor: make Spinner component stateless

--- src/ui/Spinner.jsx
+++ src/ui/Spinner.jsx
@@ -1,27 +1,13 @@
 import React, { useEffect, useState } from "react";
 
-const Spinner = ({ text = "Loading...", timer = 2000 }) => {
-  const [isOpen, setIsOpen] = useState(true);
-
-  useEffect(() => {
-    const loadingTimer = setTimeout(() => {
-      setIsOpen(false);
-    }, timer)
-
-    return () => clearTimeout(loadingTimer); 
-  }, [])
-
+const Spinner = () => {
   return (
-    <>
-      {isOpen && (
-        <section className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center">
-          <div className="flex flex-col items-center gap-4">
-            <div className="border border-blue-700 border-t-transparent w-8 h-8 rounded-full animate-spin"></div>
-            <p>{text}</p>
-          </div>
-        </section>
-      )}
-    </>
+    <section className="fixed inset-0 flex items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <div className="border border-blue-700 border-t-transparent w-8 h-8 rounded-full animate-spin"></div>
+        <p>{text}</p>
+      </div>
+    </section>
   );
 };
 
