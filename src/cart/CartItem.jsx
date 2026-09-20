0b5cbde5ff4fcade04c3a04a7a465252b1a8977e
Amiir25 <amirsadik636@gmail.com>
2026-09-20 13:47:19 +0300

    fix: fix image path error

--- src/cart/CartItem.jsx
+++ src/cart/CartItem.jsx
@@ -9,7 +9,7 @@ const CartItem = () => {
   return (
     <section className="mt-4 flex flex-col gap-8">
       {cart.map((item) => {
-        const { id, nameEn, nameAm, description, priceETB, qty } = item;
+        const { id, nameEn, nameAm, description, priceETB, qty, imagePath } = item;
         return (
           <div
             key={id}
@@ -19,7 +19,7 @@ const CartItem = () => {
             <section className="flex flex-col lg:flex-row gap-4">
               <div className="h-30 lg:h-auto w-full lg:w-40">
                 <img
-                  src="/src/assets/dish-images/doro-wet.jpg"
+                  src={`${imagePath}/image-main.png`}
                   alt=""
                   className="w-full h-full object-cover lg:object-contain"
                 />
