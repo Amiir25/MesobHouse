51841636548073faafea3ff20fa781c931def7b7
Amiir25 <amirsadik636@gmail.com>
2026-09-25 20:38:47 +0300

    style: update the top banner on home page

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -6,16 +6,17 @@ import Reflections from "./Reflections";
 import CTA from "./CTA";
 
 const Home = () => {
-
   return (
     <main className="px-2 md:px-8 lg:px-18 mt-8">
       {/* Top text */}
-      <section className="mx-2 flex items-center justify-center lg:justify-between gap-4 text-[10px] mt-4 bg-light-red p-1 rounded-2xl tracking-wider">
-        <div className="flex flex-col lg:flex-row lg:items-center gap-x-4">
-          <FaCircleCheck className="text-dark-yellow mx-12" />
-          <p className="text-dark-red">100% PURE TEFF</p>
-          <p className="text-dark-yellow">Slow-Cooked Daily</p>
-          <p>Heritage grains sourced direct fro</p>
+      <section className="mx-2 flex items-center justify-center md:justify-between gap-4 text-[10px] mt-4 bg-light-red p-1 rounded-2xl tracking-wider">
+        <div className="flex items-center gap-4 lg:ml-12">
+          <FaCircleCheck className="text-dark-yellow" />
+          <p className="flex flex-col lg:flex-row lg:items-center gap-x-4">
+            <span className="text-dark-red">100% PURE TEFF</span>
+            <span className="text-dark-yellow">Slow-Cooked Daily</span>
+            <span>Heritage grains sourced direct fro</span>
+          </p>
         </div>
         <p className="bg-green-200 text-green-800 w-fit px-4 py-1 rounded-2xl">
           Gluten Free
